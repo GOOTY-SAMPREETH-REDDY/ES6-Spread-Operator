9e7a6c191a3344f1a9e2af10cb1889f3477308ee
GOOTY-SAMPREETH-REDDY <109329775+GOOTY-SAMPREETH-REDDY@users.noreply.github.com>
2023-08-25 21:21:30 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+const arr={
+  1:"mango",
+  2:"apple",
+  3:"seck"
+};
+const arr1={
+ 
+  arr};
+
+console.log(arr1);
+
